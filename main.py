65ff3f2a155749766de4ca44b888e2c10a17d2a1
ismaheelfaheem <ismaheelfaheem@gmail.com>
2026-06-21 15:08:26 +1000

    a

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
     # Build and run the graph
     graph = build_graph()
     
-    final_state = graph.invoke({
+    final_state = graph.invoke({a
         "raw_report": raw_report,
         "agent_logs": [],
         "errors": [],
